af0e03232c9a4c15179c0e375068c6ba0014fe1b
Unikbith <1459712848@qq.com>
2026-06-13 15:47:18 +0800

    添加了项目文件夹

---
+++ web课程设计/js/验证.js
@@ -0,0 +1,13 @@
+function validateAccount() {
+            var account = document.getElementById("account").value;
+            var password = document.getElementById("password").value;
+
+            var accountPattern = /^[a-zA-Z][a-zA-Z0-9]{7,23}$/;
+            var passwordPattern = /^[a-zA-Z][a-zA-Z0-9]{7,23}$/;
+
+            if (accountPattern.test(account) && passwordPattern.test(password)) {
+                alert("登陆成功");window.location.href="../index.html";
+            } else {
+                alert("账号或密码无效！请确保账号和密码以字母开头，后跟字母或数字，长度在8-24个字符之间。");
+            }
+        }
